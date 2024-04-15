a91051b719cc3c9771bc0142a1c6dd778438e395
Marcelo Zuza <falecomzuza@gmail.com>
2024-04-15 16:03:11 -0300

    All Done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<RegistrationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+builder.Services.AddDbContext<RegistrationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao"), sqlServerOptionsAction: sqlOptions => {
+    sqlOptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+}));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
